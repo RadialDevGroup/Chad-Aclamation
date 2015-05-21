dd38df2d63c1a9072e770ae4fc771b5f71bec087
Chad Perrin <chad@radialdevgroup.com>
2015-05-21 16:17:26 +0000

    fixed nested route definition boilerplate

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -9,10 +9,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  Rails.application.routes.draw do
-    resources :articles
-    root 'welcome#index'
-  end
+  resources :articles
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
