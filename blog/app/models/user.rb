17d8fbd326a69bb70903927695ae1a1cfc167d6f
Chad Perrin <chad@radialdevgroup.com>
2015-05-26 12:49:37 +0000

    implemented login by username

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,8 +1,24 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+  validates :username, length: { in: 4..16 }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      q_string = "lower(username) = :value OR lower(email) = :value"
+      where(conditions).where([q_string, {:value => login.downcase}]).first
+    else
+      where(conditions).first
+    end
+  end
 
   # Setup accessible (or protected) attributes for your model
   #attr_accessible :email, :password, :password_confirmation, :remember_me
