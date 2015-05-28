7ec18c0f27a9ba626bfd6c83d1a31b7143318c0e
Chad Perrin <chad@radialdevgroup.com>
2015-05-28 22:39:58 +0000

    show was not used

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_user, only: [:show, :edit, :update]
+  before_action :find_user, only: [:edit, :update]
 
   def index
     @users = User.all
-  end
-
-  def show
   end
 
   def edit
